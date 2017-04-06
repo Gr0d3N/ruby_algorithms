a995c71d5076c5dd134a39130e9a29a756fb5b84
Mina Habib <minahabi@amazon.com>
2017-04-06 03:04:19 +0000

    add unique characters

---
+++ 05.unique_characters.rb
@@ -0,0 +1,12 @@
+#Question: Implement an algorithm to determine if a string has all unique characters.
+
+def unique_characters?(str)
+  chars = Hash.new()
+
+  str.length.times do |i|
+    if chars[str[i]] == true
+      return false
+    end
+    chars[str[i]] = true
+  end
+end
